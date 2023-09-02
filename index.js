34b4304f72cc150f34568ce3792eb390a956123b
Aldo Resendiz <aldoresendiz@gmail.com>
2023-09-01 23:30:55 -0500

    ready for review and feedback

--- index.js
+++ index.js
@@ -2,10 +2,35 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
 
+// my code
+// the following statement prevents the character running behind the rest of the items in our page
+character.style.zIndex = 1;
+
+function handleDirectionChange(direction) {
+    /* After making sure everything works per specifications, I refactored the code to use a ternary conditional operator. 
+    This way we accomplish the same functionality with only one line of code */
+    character.src = ('assets/green-character/' + (direction === null ? 'static' : `${direction}`) + '.gif');
+    // console.log(character.src);
+    // if (direction === null) {
+    //     character.src = 'assets/green-character/static.gif';
+    // }
+    // /* instead of writing a bunch of if's to move the character in certain direction, we just 
+    // insert the variable 'direction' into the string that we pass to the src property which is the same as the .gif file
+    // */
+    // else {
+    //     character.src = `assets/green-character/${direction}.gif`;
+    // }    
+}
+
+let direction = null;
+let x = 100;
+let y = 250;
+
+move(character).withArrowKeys(100,250, handleDirectionChange);
 
 move(newImage('assets/tree.png')).to(200, 450)
+// move(newImage('assets/tree.png')).withArrowKeys(200,450);
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
--- move.js
+++ move.js
@@ -6,7 +6,76 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
-    return {
-        to: moveToCoordinates
+    // added my code here: Aldo
+    // I used a switch to evaluate the value of 'direction' and take action.
+    // This approach makes no difference in the performance and speed for this particular project 
+    //      but ilustrates the importance of saving memory resources and improve spped and performance in bigger projects.
+    //      I also make sure it does not matter if we pass any cardinal point in lowercase, UPPERCASE, camelCase, etc. 
+    //      By converting everyting to lowercase we don't have to deal with that problem.
+    function moveWithArrowKeys(left, bottom, callback) {
+        let direction = null;
+        let x = left;
+        let y = bottom;
+        element.style.left = x + 'px';
+        element.style.bottom = y + 'px';
+
+        function moveCharacter() {
+            direction = String(direction).toLowerCase();
+            switch (direction) {
+                case 'west':
+                    x--;
+                    break;
+                case 'north':
+                    y++;
+                    break;
+                case 'east':
+                    x++;
+                    break;
+                case 'south':
+                    y--;
+                    break;
+                default:
+                    break;
+            }
+            if (x < 0) x++;
+            if (x > window.innerWidth) x--;
+            if (y < 0) y++;
+            if (y > window.innerHeight) y--;
+            element.style.left = x + 'px';
+            element.style.bottom = y + 'px';
+        }
+
+        setInterval(moveCharacter, 1);
+        document.addEventListener('keydown', function(e) {
+            if (e.repeat) return;
+            switch(e.key.toLowerCase()) {
+                case 'arrowleft':
+                    direction = 'west';
+                    break;
+                case 'arrowup':
+                    direction = 'north';
+                    break;
+                case 'arrowright':
+                    direction = 'east';
+                    break;
+                case 'arrowdown':
+                    direction = 'south';
+                    break;
+                default:
+                    break;
+            }
+            callback(direction);
+        })
+
+        document.addEventListener('keyup', function(e) {
+            direction = null;
+            callback(direction);
+        })
     }
+    // end added my code
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    };
 }
